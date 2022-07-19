53606cbda78d8b91be24ced3c1dc46b6955c3ebd
JB Burroughs <jbburroughs.dev@gmail.com>
2022-07-19 17:54:36 -0400

    dishes: list, read, methodNA

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,32 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+/* -- VALIDATION -- */
+function dishExists(req, res, next){
+    const dishId = req.params.dishId;
+    const foundDish = dishes.find(dish => dish.id === dishId);
+    if(foundDish){
+        res.locals.dish = foundDish;
+        return next();
+    }
+    next({
+        status: 404,
+        message: `Dish id not found: ${dishId}`
+    })
+}
+
+/* -- ROUTE HANDLING -- */
+function list(req, res){
+    res.json({ data: dishes})
+}
+
+function read(req, res) {
+    res.json({ data: res.locals.dish})
+}
+
+
+module.exports = {
+    list,
+    read: [dishExists, read]
+}
